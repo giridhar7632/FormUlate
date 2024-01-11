fc9c4e435112cd71f211cec77d723cec5c9890cc
Giridhar Talla <giridhar.talla2002@gmail.com>
2024-01-11 14:33:25 +0530

    optimize image loading

--- next.config.js
+++ next.config.js
@@ -22,6 +22,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'us-east-1.storage.xata.sh',
+      },
     ],
   },
   experimental: {
