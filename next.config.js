b957adf263f0a623381ceacb8c47eaf0c6983455
Giridhar Talla <giridhar.talla2002@gmail.com>
2025-12-07 21:10:03 +0530

    disable COOP

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Cross-Origin-Opener-Policy", value: "" },
+          { key: "Cross-Origin-Embedder-Policy", value: "" },
+        ],
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
